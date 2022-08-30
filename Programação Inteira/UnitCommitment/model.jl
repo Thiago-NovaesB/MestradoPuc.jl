bce018e19ca01774d9e839be7268faac56fee06a
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2022-08-30 10:34:21 -0300

    fix UC

--- Programação Inteira/UnitCommitment/model.jl
+++ Programação Inteira/UnitCommitment/model.jl
@@ -8,16 +8,15 @@ function create_variables!(prb::Problem)
     
     if options.use_commit
         @variable(model, 0 <= generation[i in 1:size.gen, 1:size.stages] <= data.g_max[i]);
+        @variable(model, comt[1:size.gen, 1:size.stages],Bin);
+        @variable(model, 0 <= turn_on[1:size.gen, 1:size.stages] <= 1);
+        @variable(model, 0 <= turn_off[1:size.gen, 1:size.stages] <= 1);
     else
         @variable(model, data.g_min[i] <= generation[i in 1:size.gen, 1:size.stages] <= data.g_max[i]);
     end
     
     @variable(model, -data.f_max[i] <= flow[i in 1:size.circ, 1:size.stages] <= data.f_max[i]);
     @variable(model, deficit[1:size.bus, 1:size.stages] >= 0.0);
-    @variable(model, comt[1:size.gen, 1:size.stages],Bin);
-    @variable(model, 0 <= turn_on[1:size.gen, 1:size.stages] <= 1);
-    @variable(model, 0 <= turn_off[1:size.gen, 1:size.stages] <= 1);
-    
 
     if options.use_kirchhoff 
         @variable(model, theta[1:size.bus, 1:size.stages]);
@@ -34,10 +33,6 @@ function create_constraints!(prb::Problem)
     generation = model[:generation];
     flow = model[:flow];
     deficit = model[:deficit];
-    comt = model[:comt];
-    turn_on = model[:turn_on];
-    turn_off = model[:turn_off];
-
 
     ################ KCL constraint 
     @constraint(model,KCL[i in 1:size.bus, t in 1:size.stages], sum(generation[j,t] for j in 1:size.gen if data.gen2bus[j] == i) +
@@ -50,19 +45,16 @@ function create_constraints!(prb::Problem)
     end
 
     ################ RAMP constraint 
-    ### olhar restrição para Gmin != 0
     if options.use_ramp &&  options.use_commit
+        comt = model[:comt];
+        turn_on = model[:turn_on];
+        turn_off = model[:turn_off];
         @constraint(model, RAMP_UP[t in 1:size.stages, g in 1:size.gen], turn_on[g,mod1(t+1,size.stages)]*data.g_max[g] + data.ramp_up[g]*comt[g,t] >= generation[g,mod1(t+1,size.stages)] - generation[g,t]);
-
         @constraint(model, RAMP_DOWN[t in 1:size.stages, g in 1:size.gen], -data.ramp_down[g]*comt[g,mod1(t+1,size.stages)] -turn_off[g,mod1(t+1,size.stages)]*data.g_min[g] <= generation[g,mod1(t+1,size.stages)] - generation[g,t]);
-
     elseif options.use_ramp
         @constraint(model, RAMP_UP[t in 1:size.stages, g in 1:size.gen], data.ramp_up[g] >= generation[g,mod1(t+1,size.stages)] - generation[g,t]);
-
         @constraint(model, RAMP_DOWN[t in 1:size.stages, g in 1:size.gen], -data.ramp_down[g] <= generation[g,mod1(t+1,size.stages)] - generation[g,t]);
     end
-
-    
 
     ################ COMMIT
     if options.use_commit
@@ -90,14 +82,13 @@ function objective_function!(prb::Problem)
     options = prb.options;
 
     generation = model[:generation];
-    flow = model[:flow];
     deficit = model[:deficit];
-    comt = model[:comt];
-    turn_on = model[:turn_on];
-    turn_off = model[:turn_off];
-
+    
+    
     FO = @expression(model, sum(generation[i,t]*data.gen_cost[i] for i in 1:size.gen, t in 1:size.stages) + sum(deficit[j,t]*data.def_cost[j] for j in 1:size.bus, t in 1:size.stages));
     if options.use_commit
+        turn_on = model[:turn_on];
+        turn_off = model[:turn_off];
         add_to_expression!(FO, sum(turn_on[g,t]*data.turn_on_cost[g] + turn_off[g,t]*data.turn_off_cost[g] for g in 1:size.gen, t in 1:size.stages))
     end
     @objective(model,Min, FO);
@@ -106,15 +97,9 @@ end
 
 function create_model!(prb::Problem)
 
-    data = prb.data;
-    size = prb.size;
     options = prb.options;
-
     prb.model = Model(options.solver)
-
     create_variables!(prb);
-
     create_constraints!(prb)
-    
     objective_function!(prb)
 end
